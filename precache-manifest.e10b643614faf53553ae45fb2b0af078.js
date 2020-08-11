8d5952990f6de50761e3c1220fa2706db5aa271d
alexmaunder <alex_maunder@hotmail.com>
2020-08-11 11:11:30 +1000

    Updates

---
+++ precache-manifest.e10b643614faf53553ae45fb2b0af078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3865b8ec0ac52a1def3f3757a037447",
+    "url": "/genelab/index.html"
+  },
+  {
+    "revision": "7ad3f98ad1c986ad39e0",
+    "url": "/genelab/static/css/2.6118100d.chunk.css"
+  },
+  {
+    "revision": "01a527d7ed7f55d531f7",
+    "url": "/genelab/static/css/main.94f70c42.chunk.css"
+  },
+  {
+    "revision": "7ad3f98ad1c986ad39e0",
+    "url": "/genelab/static/js/2.cbb6be07.chunk.js"
+  },
+  {
+    "revision": "469801ec284286c5b14f6094c74ebca7",
+    "url": "/genelab/static/js/2.cbb6be07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a527d7ed7f55d531f7",
+    "url": "/genelab/static/js/main.95fd7993.chunk.js"
+  },
+  {
+    "revision": "3bb82f7d7243ae3480ec",
+    "url": "/genelab/static/js/runtime-main.57826a52.js"
+  }
+]);
